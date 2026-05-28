949d3be13881563a0d594d4157029ae61cc9fdfe
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-05-29 00:47:28 +0700

    cek antrian kosong

--- circularquequesarray.cpp
+++ circularquequesarray.cpp
@@ -31,3 +31,10 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
